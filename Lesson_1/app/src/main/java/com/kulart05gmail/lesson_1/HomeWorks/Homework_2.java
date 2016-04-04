801f0d96d6621a00ff87e080451ff21cc9a8886d
Kul Artyom <kulart05@gmail.com>
2016-04-04 09:59:34 +0300

    +

--- Lesson_1/app/src/main/java/com/kulart05gmail/lesson_1/HomeWorks/Homework_2.java
+++ Lesson_1/app/src/main/java/com/kulart05gmail/lesson_1/HomeWorks/Homework_2.java
@@ -75,6 +75,7 @@ public class Homework_2 extends Activity {
         @Override
         public void run() {
             try {
+                Thread.sleep(2500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } ;
